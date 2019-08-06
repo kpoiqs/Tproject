d9edff02a3be268af27d9b1f45d85cfba8914935
user <user@DESKTOP-01ND8VV>
2019-08-06 15:14:28 +0900

    3:14

---
+++ Pproject/WebContent/login.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>login</title>
+</head>
+<body>
+id<input type="text"><br />
+pwd<input type="text">
+<button>login</button><br />
+<button>sign up</button>
+<button>find id</button>
+</body>
+</html>
--- Pproject/src/DBO/BaseDAO.java
+++ Pproject/src/DBO/BaseDAO.java
@@ -1,5 +1,54 @@
 package DBO;
 
-public class BaseDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class BaseDAO {
+	public static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static final String USER = "madang";
+	public static final String PASSWORD = "madang";
+	
+	public Connection getConnection() {
+		
+		Connection connection = null;
+		
+		try {
+			Class.forName(DRIVER);
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			
+		}catch(SQLException ex01) {
+			ex01.printStackTrace();
+		}catch(ClassNotFoundException ex02) {
+			ex02.printStackTrace();
+		}return connection;
+	}
+
+
+	public void CloseDBObjects(ResultSet resultSet , Statement statement , Connection connection) {
+		if(resultSet !=null) {
+			try {
+				resultSet.close();
+			}catch(SQLException a) {
+				a.printStackTrace();
+			}
+		}
+		if(statement !=null) {
+			try {
+				statement.close();
+			}catch(SQLException b) {
+				b.printStackTrace();
+			}
+		}
+		if(connection !=null) {
+			try {
+				connection.close();
+			}catch(SQLException c){
+				c.printStackTrace();
+			}
+		}
+	}
 }
